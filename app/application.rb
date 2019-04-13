97d49b7ede5931bfdd5b44ba6acd5583bb1e712d
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-13 22:12:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,5 +5,11 @@ class Application
     resp.write "Hello, World"
     resp.finish
   end
+  
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, World"
+    resp.finish
+  end
 
 end
